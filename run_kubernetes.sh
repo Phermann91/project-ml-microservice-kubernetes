1029f84aea8cda8ce768c10eaab4da8e680e06f6
phermann <philipp-hermann1@gmx.net>
2021-06-03 16:40:49 +0200

    deploy with kubernetes and save output log

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-dockerpath="noahgift/flasksklearn"
+# dockerpath="noahgift/flasksklearn"
+dockerpath="phermann91/mlmicroservice"
 
 # Run in Docker Hub container with kubernetes
-kubectl run flaskskearlndemo\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=flaskskearlndemo
+kubectl run mlmicroservice --image=$dockerpath --port=80
 
 # List kubernetes pods
 kubectl get pods
 
 # Forward the container port to host
-kubectl port-forward flaskskearlndemo 8000:80
-
+kubectl port-forward mlmicroservice 8000:80
